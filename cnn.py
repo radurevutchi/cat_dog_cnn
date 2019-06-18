4d23a1a88de9581ed8affffe892d0d0c5c1651eb
Radu Revutchi <radurevutchi@gmail.com>
2019-06-19 02:11:34 +0300

    update

---
+++ cnn.py
@@ -0,0 +1,8 @@
+
+
+
+from keras.models import Sequential # initializes NN as sequanential
+from keras.layers import Conv2D # 2D Conv. layer (3D used for video and time dimension)
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
