9a8c7616642cd0fa18965931f4951785369ac333
Radu Revutchi <radurevutchi@gmail.com>
2019-06-19 02:24:29 +0300

    update

--- cnn.py
+++ cnn.py
@@ -3,6 +3,15 @@
 
 from keras.models import Sequential # initializes NN as sequanential
 from keras.layers import Conv2D # 2D Conv. layer (3D used for video and time dimension)
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
+from keras.layers import MaxPooling2D # max pooling step
+from keras.layers import Flatten # flattens 2D images/2d arrays to a vector
 from keras.layers import Dense
+
+
+# creates sequential NN object
+classifier = Sequential()
+
+
+# adding a convolutional layer
+# 
+classifier.add(Conv2D(32, (3,3), input_shape=(64,64,3), activation='relu'))
